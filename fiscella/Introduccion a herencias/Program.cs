77de0e61afb9044434523e8085affd8e0489cb77
Tobitobit0bi <tobias.coman@hotmail.com>
2023-04-27 17:24:53 -0300

    Introduccion a herencias

---
+++ fiscella/Introduccion a herencias/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion_a_herencias
+{
+    class Empleado 
+    {
+        string nom;
+        string ape;
+        public string Nombre
+        {
+            get { return nom; } set { this.nom = value; } 
+        }
+        public string Apellido
+        {
+            get { return ape; } set { this.ape = value; }
+        }
+    }
+
+    class Diseñador:Empleado 
+    {
+    
+    }
+
+    class Backend:Empleado 
+    {
+    
+    }
+
+    class FullStack:Empleado 
+    {
+    
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Backend b = new Backend();
+
+            b.Nombre = "Elmo";
+            b.Apellido = "Mero";
+
+            Console.Write($"quiero probar esta manera de usar strings, el nombre es: {b.Nombre}, el apellido es: {b.Apellido}");
+            Console.ReadKey();
+
+        }
+    }
+}
