a8c08612b4084d13ff86d823f35b27acb99af97c
Tobitobit0bi <Tobias.coman@hotmail.com>
2023-05-05 14:11:27 -0300

    avances en ejercicio obligatorio para aprobar la materia 4

---
+++ fiscella/EOPAM ejer 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOPAM_ejer_4
+{
+    internal class Program
+    {
+        class Electrodomesticos {
+            const int precioBase = 100;
+
+            int precio = precioBase;
+            string color = "blanco";
+            string consumoEnergico = "F";
+            int peso = 5;
+
+            public Electrodomesticos() { }
+
+            public Electrodomesticos(int precio, int peso) { 
+                this.precio = precio;
+                this.peso = peso;
+            }
+            public Electrodomesticos(int precio, string color, string consumo, int peso)
+            {
+                this.precio = precio;
+                this.color = color;
+                this.consumoEnergico = consumo;
+                this.peso = peso;
+
+            }
+        }
+
+        class Lavadora : Electrodomesticos { 
+            
+        }
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
