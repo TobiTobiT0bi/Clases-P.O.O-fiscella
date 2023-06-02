4b8c30ebefe2b2e8eb809c67bcf56ef34b701236
simoncarrera <simoncarrenoe.t26@gmail.com>
2023-06-02 17:44:53 -0300

    termino ejer 7

--- fiscella/EOPAM 7/Program.cs
+++ fiscella/EOPAM 7/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+Vamos a realizar una clase llamada Raíces, donde representaremos los valores de una ecuación de 2º grado.
+Tendremos los 3 coeficientes como atributos, llamémosles a, b y c.
+Hay que insertar estos 3 valores para construir el objeto.
+Las operaciones que se podrán hacer son las siguientes:
+obtenerRaices(): imprime las 2 posibles soluciones
+obtenerRaiz(): imprime una única raíz, que será cuando solo tenga una solución posible.
+getDiscriminante(): devuelve el valor del discriminante (double), el discriminante tiene la siguiente fórmula, (b^2)-4*a*c
+tieneRaices(): devuelve un booleano indicando si tiene dos soluciones, para que esto ocurra, el discriminante debe ser mayor o igual que 0.
+tieneRaiz(): devuelve un booleano indicando si tiene una única solución, para que esto ocurra, el discriminante debe ser igual que 0.
+calcular(): mostrará por consola las posibles soluciones que tiene nuestra ecuación, en caso de no existir solución, mostrarlo también.
+Fórmula ecuación 2º grado: (-b±√((b^2)-(4*a*c)))/(2*a)
+Solo varía el signo delante de -b
+ */
 namespace EOPAM_7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Raices ecuacion;
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Ingrese el primer numero (x grado 2)");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el segundo numero (x grado 1)");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el 3er numero (grado 1)");
+                int c = Convert.ToInt32(Console.ReadLine());
+
+                ecuacion = new Raices(a, b, c);
+
+                Console.ReadKey(true);
+
+            }
         }
     }
 }
---
+++ fiscella/EOPAM 7/Raices.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOPAM_7
+{
+    internal class Raices            // ¿es dificil de leer? esto lo use para practicar ternarios ya que estaba, creo que me emocioné un poco
+    {
+        int a;
+        int b;
+        int c;
+
+        double discriminante;
+
+        public Raices(int a, int b, int c) { 
+            this.a = a; this.b = b; this.c = c;
+            discriminante = getDiscriminante();
+
+            Calcular(discriminante);
+        }
+
+        double getDiscriminante() {
+            return (b * b) - 4 * a * c;
+        }
+
+        public string obtenerRaices(double discriminante) {
+            double[] raices = new double[2];
+
+            double cuadrada = Math.Sqrt(discriminante);
+
+            raices[0] = (-b + cuadrada) / (2 * a);
+            raices[1] = (-b - cuadrada) / (2 * a);
+
+            return $"raices de {a}x{(b < 0 ? "" : "+")}{b}x{(c < 0 ? "" : "+")}{c}: x1: {raices[0]}, x2: {raices[1]}";
+        }
+
+        public string obtenerRaiz(double discriminante)
+        {
+            double raiz;
+
+            raiz = (-b + Math.Sqrt(discriminante)) / (2 * a);
+
+            return $"raices de {a}x{(b < 0 ? "" : "+")}{b}x{(c < 0 ? "" : "+")}{c}: x1: {raiz}";
+        }
+
+        public bool tieneRaices(double discriminante) {
+            bool tiene = (discriminante > 0) ? true : false;
+
+            return tiene;
+        }
+
+        public bool tieneRaiz(double discriminante)
+        {
+            bool tiene = (discriminante == 0) ? true : false;
+
+            return tiene;
+        }
+
+        public void Calcular(double discriminante)
+        {
+            Console.WriteLine($"{(tieneRaices(discriminante) ? obtenerRaices(discriminante) : (tieneRaiz(discriminante) ? obtenerRaiz(discriminante) : $"{(a > 0 ? "" : "+")}{a}x{(b < 0 ? "" : "+")}{b}x{(c < 0 ? "" : "+")}{c} no tiene raices reales"))}");
+            /* if (discriminante > 0)
+            {
+                obtenerRaices(discriminante);
+            }
+            else if (discriminante == 0)
+            {
+                obtenerRaiz(discriminante);
+            }
+            else {
+                Console.Write($"{(a > 0 ? "" : Convert.ToString(Math.Sign(a)))}{a}{Math.Sign(b)}{b}{Math.Sign(c)}{c} no tiene raices reales");
+            } */
+        }
+    }
+}
