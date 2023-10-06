c1900012ba30cc7bf4d1ae94d07e369445783fb8
Edwardvee <lautaroivan.quiroga@gmail.com>
2023-10-06 14:41:59 -0300

    termino el examen ahora si

---
+++ fiscella/Examen Formularios/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Examen_Formularios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ordenar = new System.Windows.Forms.Button();
+            this.Borrar = new System.Windows.Forms.Button();
+            this.intercambiar = new System.Windows.Forms.Button();
+            this.FirstLast = new System.Windows.Forms.Button();
+            this.NombresIzq = new System.Windows.Forms.ListBox();
+            this.NombresDer = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Ordenar
+            // 
+            this.Ordenar.Location = new System.Drawing.Point(55, 370);
+            this.Ordenar.Name = "Ordenar";
+            this.Ordenar.Size = new System.Drawing.Size(75, 23);
+            this.Ordenar.TabIndex = 0;
+            this.Ordenar.Text = "Ordenar";
+            this.Ordenar.UseVisualStyleBackColor = true;
+            this.Ordenar.Click += new System.EventHandler(this.Ordenar_Click);
+            // 
+            // Borrar
+            // 
+            this.Borrar.Location = new System.Drawing.Point(185, 370);
+            this.Borrar.Name = "Borrar";
+            this.Borrar.Size = new System.Drawing.Size(75, 23);
+            this.Borrar.TabIndex = 1;
+            this.Borrar.Text = "Borrar";
+            this.Borrar.UseVisualStyleBackColor = true;
+            this.Borrar.Click += new System.EventHandler(this.Borrar_Click);
+            // 
+            // intercambiar
+            // 
+            this.intercambiar.Location = new System.Drawing.Point(533, 370);
+            this.intercambiar.Name = "intercambiar";
+            this.intercambiar.Size = new System.Drawing.Size(75, 23);
+            this.intercambiar.TabIndex = 2;
+            this.intercambiar.Text = "intercambiar";
+            this.intercambiar.UseVisualStyleBackColor = true;
+            this.intercambiar.Click += new System.EventHandler(this.intercambiar_Click);
+            // 
+            // FirstLast
+            // 
+            this.FirstLast.Location = new System.Drawing.Point(665, 370);
+            this.FirstLast.Name = "FirstLast";
+            this.FirstLast.Size = new System.Drawing.Size(79, 23);
+            this.FirstLast.TabIndex = 3;
+            this.FirstLast.Text = "1ro <-> ultimo";
+            this.FirstLast.UseVisualStyleBackColor = true;
+            this.FirstLast.Click += new System.EventHandler(this.FirstLast_Click);
+            // 
+            // NombresIzq
+            // 
+            this.NombresIzq.FormattingEnabled = true;
+            this.NombresIzq.Location = new System.Drawing.Point(12, 12);
+            this.NombresIzq.Name = "NombresIzq";
+            this.NombresIzq.Size = new System.Drawing.Size(303, 316);
+            this.NombresIzq.TabIndex = 4;
+            // 
+            // NombresDer
+            // 
+            this.NombresDer.FormattingEnabled = true;
+            this.NombresDer.Location = new System.Drawing.Point(485, 12);
+            this.NombresDer.Name = "NombresDer";
+            this.NombresDer.Size = new System.Drawing.Size(303, 316);
+            this.NombresDer.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NombresDer);
+            this.Controls.Add(this.NombresIzq);
+            this.Controls.Add(this.FirstLast);
+            this.Controls.Add(this.intercambiar);
+            this.Controls.Add(this.Borrar);
+            this.Controls.Add(this.Ordenar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ordenar;
+        private System.Windows.Forms.Button Borrar;
+        private System.Windows.Forms.Button intercambiar;
+        private System.Windows.Forms.Button FirstLast;
+        private System.Windows.Forms.ListBox NombresIzq;
+        private System.Windows.Forms.ListBox NombresDer;
+    }
+}
+
---
+++ fiscella/Examen Formularios/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Crear una estructura con:
+//4 botones
+//2 listas con 10 nombres en cada una (Ya tienen que estar ingresados en el diseño)
+
+//1.Cuando se presiona en el primer boton ordenar los listados
+//2. Con el segundo boton borran todos los nombres que tengan hasta 5 letras
+//3. Con el tercer boton intercambiar panqueque los nombres que tengan hasta 5 letras
+//4. Intercambiar el primer y ultimo elemento de ambas listas. Tienen que quedar en el primer lugar y en el ultimo
+
+namespace Examen_Formularios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NombresDer.Items.Add("Magda");
+            NombresDer.Items.Add("Agustin");
+            NombresDer.Items.Add("Pedro");
+            NombresDer.Items.Add("Teresa");
+            NombresDer.Items.Add("Maria");
+            NombresDer.Items.Add("Erica");
+            NombresDer.Items.Add("Lautaro");
+            NombresDer.Items.Add("Juan");
+            NombresDer.Items.Add("Leandro");
+            NombresDer.Items.Add("Alex");
+            NombresDer.Items.Add("Gaston");
+            NombresDer.Items.Add("Cristina");
+
+            NombresIzq.Items.Add("Luis");
+            NombresIzq.Items.Add("Pablo");
+            NombresIzq.Items.Add("Fer");
+            NombresIzq.Items.Add("Angie");
+            NombresIzq.Items.Add("Nazareno");
+            NombresIzq.Items.Add("Quintino");
+            NombresIzq.Items.Add("Ulises");
+            NombresIzq.Items.Add("Tobi");
+            NombresIzq.Items.Add("Eduardo");
+            NombresIzq.Items.Add("Quirino");
+            NombresIzq.Items.Add("Ursula");
+            NombresIzq.Items.Add("Edward");
+        }
+
+        private void Ordenar_Click(object sender, EventArgs e)
+        {
+            bool panqueque = !NombresDer.Sorted;
+            NombresIzq.Sorted = panqueque; NombresDer.Sorted = panqueque;      //excusa para poner panqueques, es mas, la
+        }                                                                      //variable bandera esta ahi porque queria probar algo nomas
+                                                                               //anda igual asi que no pasa nada
+        private void Borrar_Click(object sender, EventArgs e)
+        {
+            Borrado();
+        }
+
+        private List<List<string>> Borrado()
+        {
+            List<List<string>> Nombres5letras = new List<List<string>>();
+
+            List<string> NombresIzq5Letras = new List<string>();
+            List<string> NombresDer5Letras = new List<string>();
+
+            for (int i = (NombresIzq.Items.Count >= NombresDer.Items.Count ? NombresIzq.Items.Count : NombresDer.Items.Count); i >= 0; i--)
+            {
+                try
+                {
+                    if (NombresIzq.Items[i].ToString().Count() < 5)
+                    {
+                        NombresIzq5Letras.Add(NombresIzq.Items[i].ToString());
+                        NombresIzq.Items.RemoveAt(i);
+                    }
+                }
+                catch { /*panqueque*/ }
+                try
+                {
+                    if (NombresDer.Items[i].ToString().Count() < 5)
+                    {
+                        NombresDer5Letras.Add(NombresDer.Items[i].ToString());
+                        NombresDer.Items.RemoveAt(i);
+                    }
+                }
+                catch { /*try catch para cortar el if en el listado mas chico, no tengo idea de si hay alguna forma mejor*/  }
+            }
+
+            Nombres5letras.Add(NombresIzq5Letras); Nombres5letras.Add(NombresDer5Letras); // Nombres[0] = izquierda, Nombres[1] = derecha
+            return Nombres5letras;
+        }
+
+        private void intercambiar_Click(object sender, EventArgs e)
+        {
+            List<List<string>> Nombres = new List<List<string>>();
+            Nombres = Borrado(); // Nombres[0] = provenientes de izquierda, Nombres[1] = provenientes de derecha, panqueques
+
+            for (int i = 0; i < Nombres[0].Count(); i++)
+            {
+                NombresDer.Items.Add(Nombres[0][i].ToString());
+            }
+
+            for (int i = 0; i < Nombres[1].Count(); i++)
+            {
+                NombresIzq.Items.Add(Nombres[1][i].ToString());
+            }
+        }
+
+        private void FirstLast_Click(object sender, EventArgs e)
+        {
+            string[] nombresIzq = { NombresIzq.Items[0].ToString(), NombresIzq.Items[NombresIzq.Items.Count - 1].ToString() };
+            string[] nombresDer = { NombresDer.Items[0].ToString(), NombresDer.Items[NombresDer.Items.Count - 1].ToString() };
+
+            NombresIzq.Items[NombresIzq.Items.Count - 1] = nombresDer[1];
+            NombresIzq.Items[0] = nombresDer[0];
+
+            NombresDer.Items[NombresDer.Items.Count - 1] = nombresIzq[1];
+            NombresDer.Items[0] = nombresIzq[0];
+        }
+    }
+}
